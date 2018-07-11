25e88b4c75937637672a29159011d4194dd4d0e5
Deandrew <deamoore07@gmail.com>
2018-07-11 11:48:55 -0500

    solved error. finish sign up screen ui design

--- app/src/main/java/bookfair/android/ui/activities/SignUpActivity.java
+++ app/src/main/java/bookfair/android/ui/activities/SignUpActivity.java
@@ -19,22 +19,10 @@ public class SignUpActivity extends BaseActivity {
     @Inject
     PreferenceManager preferenceManager;
 
-    @BindView(R.id.email_editText)
-    TextInputEditText emailTextInputEditText;
-    @BindView(R.id.facebook_login_btn)
-    FancyButton facebookLoginBtn;
-    @BindView(R.id.editText_layout)
-    TextInputLayout editTextLayout;
-    @BindView(R.id.linear_layout)
-    LinearLayout linearLayout;
-    @BindView(R.id.logo)
-    AppCompatImageView logo;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        ButterKnife.bind(this);
         applicationComponent(this).inject(SignUpActivity.this);
     }
 }
