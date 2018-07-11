55ff9a8ee21e5a76d409fbf01587f988afd0b5ae
Deandrew <deamoore07@gmail.com>
2018-07-11 18:43:59 -0500

    started writing validation function for edit text fields

--- app/src/main/java/bookfair/android/ui/activities/SignUpActivity.java
+++ app/src/main/java/bookfair/android/ui/activities/SignUpActivity.java
@@ -3,7 +3,8 @@ package bookfair.android.ui.activities;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
-import android.support.v7.widget.AppCompatImageView;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import javax.inject.Inject;
@@ -18,11 +19,63 @@ public class SignUpActivity extends BaseActivity {
 
     @Inject
     PreferenceManager preferenceManager;
+    @BindView(R.id.facebook_login_btn)
+    FancyButton facebookLoginBtn;
+    @BindView(R.id.email_editText)
+    TextInputEditText emailEditText;
+    @BindView(R.id.email_layout)
+    TextInputLayout emailLayout;
+    @BindView(R.id.fullname_editTex)
+    TextInputEditText fullNameEditTex;
+    @BindView(R.id.fullname_layout)
+    TextInputLayout fullnameLayout;
+    @BindView(R.id.username_editText)
+    TextInputEditText usernameEditText;
+    @BindView(R.id.username_layout)
+    TextInputLayout usernameLayout;
+    @BindView(R.id.password_editText)
+    TextInputEditText passwordEditText;
+    @BindView(R.id.password_layout)
+    TextInputLayout passwordLayout;
+    @BindView(R.id.signup_btn)
+    FancyButton signupBtn;
+    @BindView(R.id.linear_layout)
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
         applicationComponent(this).inject(SignUpActivity.this);
+
+        facebookLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                facebookLogin();
+            }
+        });
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signUp();
+            }
+        });
+    }
+
+    private void facebookLogin() {
+
+    }
+
+    private void signUp() {
+        editTextValidation();
+    }
+
+    private void editTextValidation() {
+        if (TextUtils.isEmpty(emailEditText.getText()) || TextUtils.isEmpty(fullNameEditTex.getText()) || TextUtils.isEmpty(usernameEditText.getText())
+                || TextUtils.isEmpty(passwordEditText.getText()))
+        {
+
+        }
     }
 }
