a1cf85e00dd3e2ef84e4ebb16c52070265124d23
Deandrew <deamoore07@gmail.com>
2018-07-11 23:01:48 -0500

    completed validation for edittext fields

--- app/src/main/java/bookfair/android/ui/activities/SignUpActivity.java
+++ app/src/main/java/bookfair/android/ui/activities/SignUpActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TextInputLayout;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import javax.inject.Inject;
 
@@ -14,6 +15,8 @@ import bookfair.android.core.PreferenceManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import mehdi.sakout.fancybuttons.FancyButton;
+
+import static bookfair.android.util.Util.isEmailValid;
 
 public class SignUpActivity extends BaseActivity {
 
@@ -72,10 +75,57 @@ public class SignUpActivity extends BaseActivity {
     }
 
     private void editTextValidation() {
-        if (TextUtils.isEmpty(emailEditText.getText()) || TextUtils.isEmpty(fullNameEditTex.getText()) || TextUtils.isEmpty(usernameEditText.getText())
-                || TextUtils.isEmpty(passwordEditText.getText()))
-        {
 
+        // Reset errors.
+        emailEditText.setError(null);
+        fullNameEditTex.setError(null);
+        usernameEditText.setError(null);
+        passwordEditText.setError(null);
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(emailEditText.getText().toString())) {
+            emailEditText.setError(getString(R.string.error_field_required));
+            focusView = emailEditText;
+            cancel = true;
+        } else if (!isEmailValid(emailEditText.getText().toString())) {
+            emailEditText.setError(getString(R.string.error_invalid_email));
+            focusView = emailEditText;
+            cancel = true;
+        }
+
+        // Check for a valid name.
+        if (TextUtils.isEmpty(fullNameEditTex.getText().toString())) {
+            fullNameEditTex.setError(getString(R.string.error_field_required));
+            focusView = fullNameEditTex;
+            cancel = true;
+        }
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(usernameEditText.getText().toString())) {
+            usernameEditText.setError(getString(R.string.error_field_required));
+            focusView = usernameEditText;
+            cancel = true;
+        }
+
+        //Check for a valid password.
+        if (TextUtils.isEmpty(passwordEditText.getText().toString())) {
+            passwordEditText.setError(getString(R.string.error_field_required));
+            focusView = passwordEditText;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            Toast.makeText(SignUpActivity.this, "Success", Toast.LENGTH_SHORT).show();
         }
     }
 }
+
--- app/src/main/java/bookfair/android/util/Util.java
+++ app/src/main/java/bookfair/android/util/Util.java
@@ -1,4 +1,15 @@
 package bookfair.android.util;
 
+import android.util.Patterns;
+
 public class Util {
+
+    public final static boolean isEmailValid (CharSequence target) {
+        if (target == null) {
+            return false;
+        }else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
 }
