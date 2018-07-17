8a8f2b89fddb3c4e05f56126f4c9546fd8e7ba01
Deandrew <deamoore07@gmail.com>
2018-07-16 20:58:29 -0500

    created date deserializer

---
+++ app/src/main/java/bookfair/android/api/GsonDateDeSerializer.java
@@ -0,0 +1,26 @@
+package bookfair.android.api;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GsonDateDeSerializer implements JsonDeserializer<Date> {
+    DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    @Override
+    public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        try {
+            return dateFormat.parse(json.getAsString());
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+
+
+}
